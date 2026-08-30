9b1b7f8483350e5bfa164aeef7737e9accf99253
blackeker <blackeker@gmail.com>
2026-08-30 18:04:10 +0300

    fix: replace deprecated pino.multidest with pino.multistream in logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -18,7 +18,7 @@ const streams = [
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
   timestamp: pino.stdTimeFunctions.isoTime
-}, pino.multidest(streams));
+}, pino.multistream(streams));
 
 export const fileLogger = pino({
   level: process.env.LOG_LEVEL || 'info',
